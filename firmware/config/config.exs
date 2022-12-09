d98427f409a4c5fc44ca50d6e2009c062c408ede
Luca Ongaro <mail@lucaongaro.eu>
2022-12-09 21:29:15 +0100

    Update stuff and fix things

--- firmware/config/config.exs
+++ firmware/config/config.exs
@@ -5,37 +5,6 @@
 # is restricted to this project.
 use Mix.Config
 
-# Customize non-Elixir parts of the firmware. See
-# https://hexdocs.pm/nerves/advanced-configuration.html for details.
-
-config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
-
-# Use shoehorn to start the main application. See the shoehorn
-# docs for separating out critical OTP applications such as those
-# involved with firmware updates.
-
-config :shoehorn,
-  init: [:nerves_runtime, :nerves_init_gadget],
-  app: Mix.Project.config()[:app]
-
-# Use Ringlogger as the logger backend and remove :console.
-# See https://hexdocs.pm/ring_logger/readme.html for more information on
-# configuring ring_logger.
-
-config :logger, backends: [RingLogger]
-
-key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
-
-config :nerves_network, :default,
-  wlan0: [
-    ssid: System.get_env("NERVES_NETWORK_SSID"),
-    psk: System.get_env("NERVES_NETWORK_PSK"),
-    key_mgmt: String.to_atom(key_mgmt)
-  ],
-  eth0: [
-    ipv4_address_method: :dhcp
-  ]
-
 config :ui, UiWeb.Endpoint,
   url: [host: "thermalcam.local"],
   http: [port: 80],
@@ -46,29 +15,6 @@ config :ui, UiWeb.Endpoint,
   pubsub: [name: Nerves.PubSub, adapter: Phoenix.PubSub.PG2],
   code_reloader: false
 
-# Authorize the device to receive firmware using your public key.
-# See https://hexdocs.pm/nerves_firmware_ssh/readme.html for more information
-# on configuring nerves_firmware_ssh.
-
-key = Path.join(System.user_home!(), ".ssh/id_rsa.pub")
-unless File.exists?(key), do: Mix.raise("No SSH Keys found. Please generate an ssh key")
-
-config :nerves_firmware_ssh,
-  authorized_keys: [
-    File.read!(key)
-  ]
-
-# Configure nerves_init_gadget.
-# See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
-
-config :nerves_init_gadget,
-  ifname: "wlan0",
-  address_method: :dhcp,
-  mdns_domain: "thermalcam.local",
-  node_name: nil,
-  node_host: :mdns_domain,
-  ssh_console_port: 22
-
 # For Devices that don't support usb gadget such as Raspberry Pi 1, 2, and 3:
 # config :nerves_init_gadget,
 #   address_method: :dhcp,
@@ -78,6 +24,10 @@ config :nerves_init_gadget,
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
 
-if File.exists?(Path.join("config", "#{Mix.Project.config[:target]}.exs")) do
-  import_config "#{Mix.Project.config[:target]}.exs"
+if Mix.target() != :host do
+  import_config "target.exs"
+end
+
+if File.exists?(Path.join("config", "#{Mix.target()}.exs")) do
+  import_config "#{Mix.target()}.exs"
 end
--- firmware/mix.exs
+++ firmware/mix.exs
@@ -1,22 +1,31 @@
 defmodule Firmware.MixProject do
   use Mix.Project
 
-  @target System.get_env("MIX_TARGET") || "host"
+  @all_targets [:rpi0, :rpi3, :rpi]
+  @app :firmware
 
   def project do
     [
-      app: :firmware,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.6",
-      target: @target,
-      archives: [nerves_bootstrap: "~> 1.0"],
-      deps_path: "deps/#{@target}",
-      build_path: "_build/#{@target}",
-      lockfile: "mix.lock.#{@target}",
+      archives: [nerves_bootstrap: "~> 1.6"],
       start_permanent: Mix.env() == :prod,
-      build_embedded: @target != "host",
+      build_embedded: Mix.target() != :host,
       aliases: [loadconfig: [&bootstrap/1]],
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host],
       deps: deps()
+    ]
+  end
+
+  def release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod
     ]
   end
 
@@ -38,29 +47,21 @@ defmodule Firmware.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nerves, "~> 1.3", runtime: false},
-      {:shoehorn, "~> 0.4"},
-      {:ring_logger, "~> 0.4"}
-    ] ++ deps(@target)
+      {:nerves, "~> 1.9", runtime: false},
+      {:shoehorn, "~> 0.6"},
+      {:ring_logger, "~> 0.4"},
+
+      {:nerves_runtime, "~> 0.6", targets: @all_targets},
+      {:nerves_init_gadget, "~> 0.5", targets: @all_targets},
+      {:ui, path: "../ui", targets: @all_targets},
+
+      {:nerves_system_rpi, "~> 1.21.1", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.21.1", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.21.1", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.21.1", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi4, "~> 1.21.1", runtime: false, targets: :rpi4},
+      {:nerves_system_bbb, "~> 2.3", runtime: false, targets: :bbb},
+      {:nerves_system_x86_64, "~> 1.21.1", runtime: false, targets: :x86_64}
+    ]
   end
-
-  # Specify target specific dependencies
-  defp deps("host"), do: []
-
-  defp deps(target) do
-    [
-      {:nerves_runtime, "~> 0.6"},
-      {:nerves_init_gadget, "~> 0.5"},
-      {:ui, path: "../ui"}
-    ] ++ system(target)
-  end
-
-  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.0", runtime: false}]
-  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.0", runtime: false}]
-  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.0", runtime: false}]
-  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.0", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.0", runtime: false}]
-  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.0", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.0", runtime: false}]
-  defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
 end
